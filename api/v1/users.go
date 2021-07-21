506abdc298464ff59ba1557920434736cf8b9373
Flynn <flynnsun@gainmiles-tw.com>
2021-07-21 18:10:01 +0800

    [Features]Add swagger and init files

--- api/v1/users.go
+++ api/v1/users.go
@@ -2,10 +2,18 @@ package v1
 
 import (
 	"NASDAQ_Slot_Machine/controller"
+	_ "NASDAQ_Slot_Machine/docs"
 	"github.com/gin-gonic/gin"
 )
 
-func RouteUsers(r *gin.RouterGroup) {
+// RouteUsers @Summary 說Hello
+// @Id 1
+// @Tags Hello
+// @version 1.0
+// @produce text/plain
+// @Success 200 string string 成功後返回的值
+// @Router /hello [get]
+func RouteUsers(r *gin.Engine) {
 	posts := r.Group("/ypa")
 	{
 		posts.GET("/users/:id", controller.NewUsersController().GetOne)
--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	v1 "NASDAQ_Slot_Machine/api/v1"
 	"NASDAQ_Slot_Machine/database"
+	_ "NASDAQ_Slot_Machine/docs"
 	"NASDAQ_Slot_Machine/models"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 	"os"
 )
 
 
+// @title Gin swagger
+// @version 1.0
+// @description Gin swagger
+
+// @contact.name Flynn Sun
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// schemes http
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -38,6 +53,9 @@ func main() {
 			"message": "hello " + name,
 		})
 	})
+	v1.RouteUsers(app)
+	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json")
+	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 	err2 := app.Run(":" + port)
 	if err2 != nil {
 		panic(err2)
