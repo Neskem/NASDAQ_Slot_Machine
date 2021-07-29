1a5039a72302b8baadb6320b54dd351dc44b4694
Flynn <flynnsun@gainmiles-tw.com>
2021-07-29 19:26:22 +0800

    [Features]Add X-Request middleware

--- route/v1/users.go
+++ route/v1/users.go
@@ -5,11 +5,22 @@ import (
 	_ "NASDAQ_Slot_Machine/docs"
 	"NASDAQ_Slot_Machine/middleware"
 	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
 )
+
+
+func RequestIDMiddleware(ctx *gin.Context) {
+	uuidV4 := uuid.NewV4()
+	ctx.Header("X-Request-Id", uuidV4.String())
+
+	ctx.Next()
+}
+
 
 
 func RouteUsers(r *gin.Engine) {
 	posts := r.Group("/users")
+	posts.Use(RequestIDMiddleware)
 	{
 		posts.POST("/register/", controller.NewUsersController().RegisterOne)
 		posts.POST("/login/", controller.OldUsersController().LoginOne)
@@ -17,6 +28,7 @@ func RouteUsers(r *gin.Engine) {
 
 	auth := r.Group("/users/auth")
 	auth.Use(middleware.JWTAuth())
+	auth.Use(RequestIDMiddleware)
 	{
 		auth.GET("/:id", controller.NewUsersController().GetOne)
 	}
